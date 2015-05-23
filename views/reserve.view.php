39a3b34ca1c2488916158b4fa1e92cf2d6ec6c98
a.panapadeatchy <a.panapadeatchy@rt-iut.re>
2015-05-24 01:38:51 +0400

    add js to reserve.php

--- views/reserve.view.php
+++ views/reserve.view.php
@@ -44,4 +44,14 @@
 	</div>
 </div>
 
+<!--Import jQuery before materialize.js-->
+	<script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+	<script type="text/javascript" src="assets/js/materialize.js"></script>
+
+    <script>
+        $(document).ready(function(){
+            $(".button-collapse").sideNav();
+        });
+    </script>
+
 <?php include('partials/footer.php'); ?>
