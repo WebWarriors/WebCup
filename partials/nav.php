8dc951b287ded77b78e0b8c4c0c13e1374e1fd17
a.panapadeatchy <a.panapadeatchy@rt-iut.re>
2015-05-24 01:40:52 +0400

    Edit nav

--- partials/nav.php
+++ partials/nav.php
@@ -2,32 +2,18 @@
 	<nav class="light-blue lighten-1" role="navigation">
     	<div class="nav-wrapper container">
     		<a id="logo-container" href="index.php" class="brand-logo"><?php echo WEBSITE_NAME ?></a>
+            <a href="#" data-activates="mobile-demo" class="button-collapse"><i class="mdi-navigation-menu"></i></a>
 	      	<ul class="right hide-on-med-and-down">
 	        	<li class="set_active('reserve')"><a  href="reserve.php">Réservez-votre place</a></li>
-	      	</ul>
-            <?php if(is_logged_in()): ?>
-                <ul class="right hide-on-med-and-down">
-                    <li class="set_active('account')"><a  href="account.php">Mon compte</a></li>
-                </ul>
-                <ul class="right hide-on-med-and-down">
-                    <li><a href="logout.php">Déconnexion</a></li>
-                </ul>
-            <?php else: ?>
-                <ul class="right hide-on-med-and-down">
-                    <li class="<?= set_active('login'); ?>"><a href="login.php">Inscription|Connexion</a></li></ul>
-                <ul class="right hide-on-med-and-down"> 
-            <?php endif; ?>
-
-	      	<ul id="nav-mobile" class="side-nav">
-	        	<li class="set_active('reserve')"><a  href="reserve.php">Réservez-votre place</a></li>
-	      	</ul>
             <?php if(is_logged_in()): ?>
                     <li class="set_active('account')"><a  href="account.php">Mon compte</a></li>
                     <li><a href="logout.php">Déconnexion</a></li>
+                </ul>
             <?php else: ?>
-                    <li class="<?= set_active('login'); ?>"><a href="login.php">Inscription|Connexion</a></li></ul> 
+                    <li class="<?= set_active('login'); ?>"><a href="login.php">Inscription|Connexion</a></li>
+                </ul>
             <?php endif; ?>
-	      	<a href="#" data-activates="nav-mobile" class="button-collapse"><i class="mdi-navigation-menu"></i></a>
-    	</div>
+            
+        </div>
   	</nav>
 </div>
