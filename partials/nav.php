dff7ee27fe7c0fb77de6033f928756b8725d7aa2
a.panapadeatchy <a.panapadeatchy@rt-iut.re>
2015-05-24 01:01:12 +0400

    edit nav

--- partials/nav.php
+++ partials/nav.php
@@ -14,13 +14,19 @@
                 </ul>
             <?php else: ?>
                 <ul class="right hide-on-med-and-down">
-                  <li class="<?= set_active('login'); ?>"><a href="login.php">Inscription|Connexion</a></li></ul>
+                    <li class="<?= set_active('login'); ?>"><a href="login.php">Inscription|Connexion</a></li></ul>
                 <ul class="right hide-on-med-and-down"> 
             <?php endif; ?>
 
 	      	<ul id="nav-mobile" class="side-nav">
 	        	<li class="set_active('reserve')"><a  href="reserve.php">Réservez-votre place</a></li>
 	      	</ul>
+            <?php if(is_logged_in()): ?>
+                    <li class="set_active('account')"><a  href="account.php">Mon compte</a></li>
+                    <li><a href="logout.php">Déconnexion</a></li>
+            <?php else: ?>
+                    <li class="<?= set_active('login'); ?>"><a href="login.php">Inscription|Connexion</a></li></ul> 
+            <?php endif; ?>
 	      	<a href="#" data-activates="nav-mobile" class="button-collapse"><i class="mdi-navigation-menu"></i></a>
     	</div>
   	</nav>
